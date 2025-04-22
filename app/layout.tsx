e640ecaa659b889ed87d2bdcecb3b221dcbdb76f
devsangho <gaonmail@cau.ac.kr>
2025-04-22 13:15:40 +0900

    feat: add Vercel Analytics integration for enhanced performance tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import { DataProvider } from "./contexts/data-context";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Analytics />
         <DataProvider>
           {children}
           <Toaster richColors closeButton position="top-right" />
